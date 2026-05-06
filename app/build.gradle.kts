40a3da644a87d685020572d0d1ee5b3850a92ad5
Ojaswakesharwani <ojaswakesharwani@gmail.com>
2026-05-07 02:57:50 +0530

    Configured Maven publishing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.kotlin.android)
+    id("maven-publish")
 }
 
 android {
@@ -31,9 +32,12 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "11"
+        jvmTarget = "17"
     }
 }
 
